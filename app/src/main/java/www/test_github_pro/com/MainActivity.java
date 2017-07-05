5c6765196d5c188489198777ebaf73e296a04cfc
jiangcheng@gain-bao.com <jiangcheng@gain-bao.com>
2017-07-05 15:52:51 +0800

    Initial commit

---
+++ app/src/main/java/www/test_github_pro/com/MainActivity.java
@@ -0,0 +1,13 @@
+package www.test_github_pro.com;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
