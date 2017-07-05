8e4e1fa29b62fdf5b849e137bc7cbadb4bd1cfe1
jiangcheng@gain-bao.com <jiangcheng@gain-bao.com>
2017-07-05 16:00:31 +0800

    添加了一个类

---
+++ app/src/main/java/www/test_github_pro/com/HelloGithub.java
@@ -0,0 +1,16 @@
+package www.test_github_pro.com;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by little_bug on 2017/7/5.
+ */
+public class HelloGithub extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        System.out.println("savedInstanceState = [" + savedInstanceState + "]");
+    }
+}
